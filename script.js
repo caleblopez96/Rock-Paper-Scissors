0d648d05cdc15fdda6ece6f594f540879312216b
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-01-09 20:13:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const selectionButtons = document.querySelectorAll('[data-selection]');
+const resultsText = document.querySelector('.resultsText');
 const finalColumn = document.querySelector('[data-final-column]');
-const computerScoreSpan = document.querySelector('[data-computer-score]')
+const computerScoreSpan = document.querySelector('[data-computer-score]');
 const yourScoreSpan = document.querySelector('[data-your-score]');
 const SELECTIONS = [
 {
@@ -23,7 +24,7 @@ const SELECTIONS = [
 ];
 
 selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
+selectionButton.addEventListener('click', e => {
     const selectName = selectionButton.dataset.selection;
     const selection = SELECTIONS.find(selection => selection.name === selectName);
     makeSelection(selection);
@@ -36,13 +37,23 @@ const makeSelection = (selection) => {
     const computerWinner = isWinner(computerSelection, selection);
     addSelectionResults(computerSelection, computerWinner);
     addSelectionResults(selection, yourWinner);
-    if (yourWinner) incrementScore(yourScoreSpan)
-    if (computerWinner) incrementScore(computerScoreSpan)
+    if (yourWinner) {
+        incrementScore(yourScoreSpan);
+        resultsText.innerText = "you win :)";
+        resultsText.style.fontWeight = 'bold';
+        resultsText.style.color = '#00ff7f';
+};
+    if (computerWinner) {
+        incrementScore(computerScoreSpan)
+        resultsText.innerText = "you lose :( ... try again";
+        resultsText.style.fontWeight = 'normal';
+        resultsText.style.color = '#252525'
+    };
 };
 
 const incrementScore = (scoreSpan) => {
     scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-}
+};
 
 const addSelectionResults = (selection, winner) => {
     const div = document.createElement('div');
@@ -57,6 +68,6 @@ const isWinner = (selection, opponentSelection) => {
 };
 
 const randomSelection = () => {
-const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
     return SELECTIONS[randomIndex];
 };
